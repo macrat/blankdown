8af299f147d436b470666d7af72eae87c1966bf3
MacRat <m@crat.jp>
2017-12-27 18:04:15 +0900

    Fixed ToC widget sizing

--- client/MarkdownEditor/index.vue
+++ client/MarkdownEditor/index.vue
@@ -70,10 +70,12 @@
 	display: inline-block;
 	background-color: #eaf0f0;
 	padding: .5em 1em;
-	min-width: 20em;
 	max-width: 100%;
 	position: relative;
 	box-sizing: border-box;
+}
+@media screen and (min-width: 20em) {
+	min-width: 20em;
 }
 .toc-widget:before {
 	content: ' ';
